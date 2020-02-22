310a5d01f407ae4b7d4cc05809a04a3411a65011
AislynnEdmiston <davidandaislynn@gmail.com>
2020-02-22 14:56:19 -0500

    imported actions route

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 
 // Router imports go here
+const projectRouter = require("./routes/projects");
+const actionRouter = require("./routes/actions")
 
 const server = express();
 
 server.use(morgan("short"));
 server.use(helmet());
 server.use(express.json());
+
+server.use('/api/projects', projectRouter);
+server.use('/api/actions', actionRouter);
 
 server.get("/", (req, res) => {
     res.send(`<h2>Server is working now get coding and do your magic!!!</h2>`)
